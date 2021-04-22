24c003355f4c7633f7b909e4f0ad6876b8c481c2
iamkevn <iamkevnrodriguez@gmail.com>
2021-04-22 03:07:43 -0400

    laptop large bp complete

--- javascript/home.js
+++ javascript/home.js
@@ -6,8 +6,8 @@ const navList = document.querySelector('.js-nav__list');
 const displayMenu = () => {
   navList.classList.toggle('active');
   navList.classList.contains('active')
-    ? (navMenu.src = 'images/navigation.svg')
-    : (navMenu.src = 'images/close.svg');
+    ? (navMenu.src = 'images/close.svg')
+    : (navMenu.src = 'images/navigation.svg');
 };
 
 navMenu.addEventListener('click', displayMenu);
